68936f7fc519e536e51580f516ad8c6ddad7f2a0
Anton Baumann <anton@antonbaumann.com>
2022-06-22 23:46:46 +0200

    add imagekit

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,19 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'ik.imagekit.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'ik.imagekit.io'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+},
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
