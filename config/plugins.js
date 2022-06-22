ef82758fbea05b676cf60d49bc522cf622edfb84
Anton Baumann <anton@antonbaumann.com>
2022-06-22 23:46:59 +0200

    add imagekit

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({env}) => ({
+    upload: {
+      config: {
+        provider: "strapi-provider-upload-imagekit",
+        providerOptions: {
+          publicKey: env('IMAGEKIT_PUBLIC_KEY'),
+          privateKey: env('IMAGEKIT_PRIVATE_KEY'),
+          urlEndpoint: `https://ik.imagekit.io/${env('IMAGEKIT_ID')}`,
+          params: {
+            folder: "/bifolia"
+          }
+        }
+      }
+    }
+  });
